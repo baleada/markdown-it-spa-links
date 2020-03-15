55b6e8a65366bb894946d07b510bae48fd9cd12c
AlexVipond <alex.r.vipond@gmail.com>
2020-03-15 12:38:31 -0600

    clean up

--- scripts/babelify.js
+++ scripts/babelify.js
@@ -1,8 +1,5 @@
 const { exec, empty } = require('@baleada/prepare')
 
 module.exports = function(dependency) {
-  // fs.unlinkSync('index.js')
-  // fs.unlinkSync('webpack.js')
-  // fs.unlinkSync('rollup.js')
   exec(`npx babel src/${dependency}.js -o ${dependency}.js`)
 }
